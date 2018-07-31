038cd61d957dfe43ac85aec407c449f99956edbb
Timo Runge <me@timorunge.com>
2018-07-31 13:59:19 +0200

    * Remove verbose output for travis builds

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 printf '[defaults]\nroles_path=/etc/ansible/roles' > ansible.cfg
 ansible-lint /etc/ansible/roles/${ENV_ROLE_NAME}/tasks/main.yml
-ansible-playbook test.yml -i inventory --syntax-check
-ansible-playbook test.yml -i inventory --connection=local --become -vvvv
-ansible-playbook test.yml -i inventory --connection=local --become | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+ansible-playbook ${ENV_WORKDIR}/test.yml -i ${ENV_WORKDIR}/inventory --syntax-check
+ansible-playbook ${ENV_WORKDIR}/test.yml -i ${ENV_WORKDIR}/inventory --connection=local --become $(test -z ${TRAVIS} && echo '-vvvv')
+ansible-playbook ${ENV_WORKDIR}/test.yml -i ${ENV_WORKDIR}/inventory --connection=local --become | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 REAL_NGINX_VERSION=$(/usr/local/nginx/sbin/nginx -v 2>&1) | awk -F '/' '{print $2}'
 EXPECTED_NGINX_VERSION=$(awk '/custom_nginx_version/{print $2}' ${ENV_WORKDIR}/test.yml)
 test "${REAL_NGINX_VERSION} == ${EXPECTED_NGINX_VERSION}" && (echo 'nginx version test: pass' && exit 0) || (echo 'nginx version test: fail' && exit 1)
