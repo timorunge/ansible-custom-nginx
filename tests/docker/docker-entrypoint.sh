2e8a76b74d266e61c68882eebd95c02edd52577b
Timo Runge <me@timorunge.com>
2018-09-04 16:24:44 +0200

    * Making the docker-entrypoint prettier

--- tests/docker/docker-entrypoint.sh
+++ tests/docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-printf '[defaults]\nroles_path=/etc/ansible/roles' > ansible.cfg
+printf "[defaults]\nroles_path=/etc/ansible/roles\n" > ansible.cfg
 
 ansible-lint /ansible/test.yml
 ansible-lint /etc/ansible/roles/${ansible_role}/tasks/main.yml
@@ -23,18 +23,18 @@ ansible-playbook /ansible/test.yml \
   --connection=local \
   --become \
   -e "{ custom_nginx_version: ${custom_nginx_version} }" | \
-  grep -q 'changed=0.*failed=0' && \
-  (echo 'Idempotence test: pass' && exit 0) || \
-  (echo 'Idempotence test: fail' && exit 1)
+  grep -q "changed=0.*failed=0" && \
+  (echo "Idempotence test: pass" && exit 0) || \
+  (echo "Idempotence test: fail" && exit 1)
 
 tmpfile=$(mktemp /tmp/${ansible_role}_XXXXXX)
 /usr/local/nginx/sbin/nginx -v > ${tmpfile} 2>&1
-real_nginx_version=$(cat ${tmpfile} | awk -F '/' '{print $2}')
+real_nginx_version=$(cat ${tmpfile} | awk -F "/" '{print $2}')
 rm ${tmpfile}
 test "${real_nginx_version}" = "${custom_nginx_version}" && \
-  (echo 'nginx version test: pass' && exit 0) || \
-  (echo 'nginx version test: fail' && exit 1)
+  (echo "nginx version test: pass" && exit 0) || \
+  (echo "nginx version test: fail" && exit 1)
 
 curl -Iso /dev/null http://localhost:80 && \
-  (echo 'nginx response test: pass' && exit 0) || \
-  (echo 'nginx response test: fail' && exit 1)
+  (echo "nginx response test: pass" && exit 0) || \
+  (echo "nginx response test: fail" && exit 1)
